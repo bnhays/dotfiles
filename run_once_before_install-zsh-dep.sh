0d7bc64a56105f16eaeea82ee69ead62075dad79
Nora Hays <nora@luggle.com>
2026-04-15 13:33:36 -0700

    Added zsh/oh-my-zsh/p10k bootstrap script

---
+++ run_once_before_install-zsh-dep.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -eu
+
+if command -v apt >/dev/null 2>&1; then
+  sudo apt update
+  sudo apt install -y zsh git curl
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  git clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh"
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+    "$HOME/.oh-my-zsh/custom/themes/powerlevel10k"
+fi
