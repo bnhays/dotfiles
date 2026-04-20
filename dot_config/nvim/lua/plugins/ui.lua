8f19ffb75d7e50bc272bae8781586980c772ab3a
Nora Hays <nora@luggle.com>
2026-04-20 10:13:31 -0700

    Update .config/nvim/lua/plugins/ui.lua

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,30 +1,27 @@
 return {
-  {
-    'folke/which-key.nvim',
-    event = 'VimEnter',
-    opts = {
-      delay = 0,
-      icons = { mappings = vim.g.have_nerd_font },
-      spec = {
-        { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
-        { '<leader>t', group = '[T]oggle' },
-        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-        { 'gr', group = 'LSP Actions', mode = { 'n' } },
-      },
-    },
-  },
-
-  {
-    'folke/tokyonight.nvim',
-    priority = 1000,
-    config = function()
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false },
-        },
-      }
-
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
-  },
+	{
+		"folke/which-key.nvim",
+		event = "VimEnter",
+		opts = {
+			delay = 0,
+			icons = { mappings = vim.g.have_nerd_font },
+			spec = {
+				{ "<leader>s", group = "[S]earch", mode = { "n", "v" } },
+				{ "<leader>t", group = "[T]oggle" },
+				{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+				{ "gr", group = "LSP Actions", mode = { "n" } },
+			},
+		},
+	},
+	{
+		"ember-theme/nvim",
+		name = "ember",
+		priority = 1000,
+		config = function()
+			require("ember").setup({
+				variant = "ember", -- "ember" | "ember-soft" | "ember-light"
+			})
+			vim.cmd("colorscheme ember")
+		end,
+	},
 }
