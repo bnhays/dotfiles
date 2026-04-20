979497540913a61cc24f69058813ac9f4d405b2c
Nora Hays <nora@luggle.com>
2026-04-20 10:37:47 -0700

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -32,9 +32,8 @@ vim.o.smartcase = true
 -- Keep signcolumn on by default
 vim.o.signcolumn = "yes"
 
--- Set shell to interactive zsh
+-- Set shell to zsh
 vim.o.shell = "/bin/zsh"
-vim.o.shellcmdflag = "-ic"
 
 -- Decrease update time
 vim.o.updatetime = 250
